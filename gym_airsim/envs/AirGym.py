7944cb18ecf641712e52ad1e8e7565b75701e36f
omritz <omritzur91@gmail.com>
2020-07-18 19:56:55 +0300

    update reward

--- gym_airsim/envs/AirGym.py
+++ gym_airsim/envs/AirGym.py
@@ -8,6 +8,7 @@ logger = logging.getLogger(__name__)
 
 airSimClient = myAirSimClient()
 
+
 class AirSimEnv(gym.Env):
 
     def __init__(self):
@@ -19,8 +20,8 @@ class AirSimEnv(gym.Env):
         # self.obstacle_list = [[0.0, -7.0], [0.0, 5.0], [10.0, -7.0], [10.0, -1.0], [10.0, 5.0], [5, -3.0], [5.0, 3.0]]
         # index = np.random.randint(0, len(self.goal_list) - 1)
         # self.goal = self.goal_list[index]  # global xy coordinates
-        self.x_goal = random.uniform(-3, 17)
-        self.y_goal = random.uniform(-11, 9)
+        self.x_goal = random.uniform(-2, 16)
+        self.y_goal = random.uniform(-10, 8)
         self.goal = [self.x_goal, self.y_goal]
         self.episodeN = 0
         self.stepN = 0
@@ -35,19 +36,14 @@ class AirSimEnv(gym.Env):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
-    def computeReward(self, now, track_now, distance):
+    def computeReward(self, distance, alpha, beta):
 
         distance_now = distance
         distance_before = self.allLogs['distance'][-1]
-
-        r = 0
-
-        if self.stepN > 5 and len(set(self.allLogs['distance'][len(self.allLogs['distance']):len(self.allLogs['distance'])-5:-1])) == 1:
-            r = r - 100
-        else:
-            r = -1 + (distance_before - distance_now)
-
-        return r
+        delta_distance = distance_now - distance_before
+        if self.stepN > 8 and len(set(self.allLogs['distance'][len(self.allLogs['distance']):len(self.allLogs['distance']) - 8:-1])) == 1:
+            beta = 1
+        return 1000 * alpha - 100 * beta - 20 * delta_distance - 1
 
     def step(self, action):
         assert self.action_space.contains(action), "%r (%s) invalid" % (action, type(action))
@@ -55,50 +51,65 @@ class AirSimEnv(gym.Env):
         self.addToLog('action', action)
 
         self.stepN += 1
-
         collided = airSimClient.take_action(action)
-
         now = airSimClient.getPosition()
         # print('X % s' % now.x_val)
         track = airSimClient.goal_direction(self.goal, now)
-        print('track % s' % track)
+        # print('track % s' % track)
         self.dis = np.sqrt(np.power((self.goal[0] - now.x_val), 2) + np.power((self.goal[1] - now.y_val), 2))
-        if collided:
-            done = True
-            reward = -50.0
-        elif collided == 99:
-            done = True
-            reward = 0.0
-        else:
-            done = False
-            reward = self.computeReward(now, track, self.dis)
-
-        # You made it
+        alpha = 0
+        beta = 0
+        done = False
         if self.dis < 1:
             done = True
-            reward = 100.0
+            # reward = 1000.0
+            alpha = 1
+        elif self.stepN >= 300:
+            done = True
+            # reward = -100.0
+            beta = 1
+        elif collided == True:
+            done = True
+            # reward = -100.0
+            # self.success = False
+            beta = 1
+        reward = self.computeReward(self.dis, alpha, beta)
+        # if collided:
+        #     done = True
+        #     reward = -100.0
+        # elif collided == 99:
+        #     done = True
+        #     reward = 0.0
+        # else:
+        #     done = False
+        #     reward = self.computeReward(self.dis)
+        #
+        # # You made it
+        # if self.dis < 1:
+        #     done = True
+        #     reward = 100.0
 
         self.addToLog('reward', reward)
-        rewardSum = np.sum(self.allLogs['reward'])
         self.addToLog('distance', self.dis)
         self.addToLog('track', track)
-
-        # Terminate the episode on large cumulative amount penalties,
-        # since drone probably got into an unexpected loop of some sort
-        if rewardSum < -100:
-            done = True
-
         info = {"x_pos": now.x_val, "y_pos": now.y_val}
         self.state = airSimClient.get_state_from_sim(track, self.dis, now)
-        print(info)
-        print('Reward for this action: %s'% reward)
-        print('Distance to target: %s' % self.dis)
+
         return self.state, reward, done, info
 
     def addToLog(self, key, value):
         if key not in self.allLogs:
             self.allLogs[key] = []
         self.allLogs[key].append(value)
+
+
+    def get_path(self):
+        current_position = [0, 0, 0]
+        pos = airSimClient.getPosition()
+        current_position[0] = pos.x_val
+        current_position[1] = pos.y_val
+        current_position[2] = -pos.z_val
+        return current_position
 
     def reset(self):
         """
@@ -108,13 +119,6 @@ class AirSimEnv(gym.Env):
             observation (object): The initial observation of the space. Initial reward is assumed to be 0.
         """
         airSimClient.AirSim_reset()
-
-        self.stepN = 0
-        self.episodeN += 1
-        self.allLogs = {'reward': [0], 'distance': [self.dis], 'track': [-2], 'action': [1]}
-        # index = np.random.randint(0, len(self.goal_list) - 1)
-        # self.goal = self.goal_list[index]  # global xy coordinates
-        # self.goal = [0.0, 4.0]
         self.x_goal = random.uniform(-3, 17)
         self.y_goal = random.uniform(-11, 9)
         self.goal = [self.x_goal, self.y_goal]
@@ -123,4 +127,7 @@ class AirSimEnv(gym.Env):
         track = airSimClient.goal_direction(self.goal, now)
         self.dis = np.sqrt(np.power((self.goal[0] - now.x_val), 2) + np.power((self.goal[1] - now.y_val), 2))
         self.state = airSimClient.get_state_from_sim(track, self.dis, now)
+        self.stepN = 0
+        self.episodeN += 1
+        self.allLogs = {'reward': [0], 'distance': [self.dis], 'track': [track], 'action': [1]}
         return self.state
